94e713e712ea2955723110d94c36f39935c7d0c5
Yrrah <harry.dfh@gmail.com>
2017-04-23 02:45:04 +0200

    commit nr 2 on this branch. Unfinished methods in AAC.

--- app/src/main/java/com/example/yrrah/yrrahmoney/addAndConfigureActivity.java
+++ app/src/main/java/com/example/yrrah/yrrahmoney/addAndConfigureActivity.java
@@ -3,6 +3,8 @@ package com.example.yrrah.yrrahmoney;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class addAndConfigureActivity extends AppCompatActivity {
 
@@ -15,5 +17,17 @@ public class addAndConfigureActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         buttonChoice = intent.getExtras().getInt("choice");
+    }
+
+    /**
+     * When a person has pressed a categoryAdd button, they get to this AAC-activity
+     * and this method is adding what the user choosed to add to that perticular category.
+     *
+     * numberToAdd - This is the number the user typed in the small number field.
+     * @param view - I dont actually know what this is.
+     */
+    public void addConfigureNumber(View view){
+        EditText editNumberToAdd = (EditText) findViewById(R.id.numberToAddAndConfigure);
+        int numberToAdd = Integer.parseInt(editNumberToAdd.getText().toString());
     }
 }
