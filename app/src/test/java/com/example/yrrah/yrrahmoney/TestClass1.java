3ad84d18c2c998969d5c5f9d2cb65d8cf003528f
Yrrah <harry.dfh@gmail.com>
2017-07-27 23:04:02 +0200

    First testBranch commit.

---
+++ app/src/test/java/com/example/yrrah/yrrahmoney/TestClass1.java
@@ -0,0 +1,17 @@
+package com.example.yrrah.yrrahmoney;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * First attempt at creating TestClasses
+ *
+ * Created by Yrrah on 2017-07-27.
+ */
+public class TestClass1 {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
