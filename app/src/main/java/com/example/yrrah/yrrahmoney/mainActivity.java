48bc1e16e571b2142698ca892f3fba4ee563b8b4
David Harald <David.Harald@sigma.se>
2018-01-18 11:22:29 +0100

    QuickAdd logic implemented. Not very well tough. Included some TODOs.

--- app/src/main/java/com/example/yrrah/yrrahmoney/mainActivity.java
+++ app/src/main/java/com/example/yrrah/yrrahmoney/mainActivity.java
@@ -50,11 +50,6 @@ public class mainActivity extends AppCompatActivity {
         }
     }
 
-    /**
-     * Populate the listview on mainActivity, showing stats from previous month abd current stats
-     * from this month.
-     * @param view - The android view
-     */
     public void stat2ButtonPressed(View view){ // TODO : Change name!!! If I decide to use this method.
         // stat2Button Pressed...
         Toast.makeText(getApplicationContext(),"stat2ButtonPressed", Toast.LENGTH_SHORT).show();
@@ -65,21 +60,24 @@ public class mainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void quickAdd(View view){ // TODO : Add quickAdd functionality
+    public void quickAdd(View view){
 
         EditText quickAddNr = (EditText) findViewById(R.id.quickAddNr); // amount added in quickAdd
-        String test = quickAddNr.getText().toString();
-        if(!test.equals("")){
-            int quickAddInt = Integer.parseInt(test);
+        String nrInput = quickAddNr.getText().toString();
+        if(!nrInput.equals("")){
+            int quickAddInt = Integer.parseInt(nrInput)*(-1);
+            DBHandler dbHandler = new DBHandler(this);
+
+            SubAmountModel sm = new SubAmountModel(1,quickAddInt,"QuickAdd",spinnerCategory,20180106); // TODO : Fix current date.
+            dbHandler.addSubAmount(sm);
+
+            quickAddNr.setText("");
+            Toast.makeText(getApplicationContext(),spinnerCategory + " added!", Toast.LENGTH_SHORT).show();
+
+            updateMainDisplayBoard(quickAddInt);
         }else{
             Toast.makeText(getApplicationContext(),"No value added", Toast.LENGTH_SHORT).show();
         }
-    }
-
-    // Not supposed to exist when app is finished.
-    public void testDB2(View view){
-        DBHandler dbHandler = new DBHandler(this);
-        dbHandler.populateDatabaseWithData();
     }
 
     private void setup(){
@@ -90,22 +88,18 @@ public class mainActivity extends AppCompatActivity {
         Spinner dropDownList = (Spinner) findViewById(R.id.quickAddSpinner);
 
         DBHandler dbHandler = new DBHandler(this);
-        // ===== ===== TEST DATA ===== =====
-        CategoryModel cm = new CategoryModel("Income", 0);
-        dbHandler.addCategory(cm);
-        cm = new CategoryModel("Expenditure", 0);
-        dbHandler.addCategory(cm);
-
-        SubAmountModel sm = new SubAmountModel(1,200,"Vinst","Income",20180106);
-        dbHandler.addSubAmount(sm);
-        sm = new SubAmountModel(2,-150,"test","Expenditure",20180106);
-        dbHandler.addSubAmount(sm);
-        sm = new SubAmountModel(3,20000,"Lön","Income",20180106);
-        dbHandler.addSubAmount(sm);
-        // ===== ===== TEST DATA ===== =====
 
         List<CategoryModel> categoryModelList = dbHandler.getAllCategories();
         List<SubAmountModel> transactionList = dbHandler.getAllSubAmounts();
+
+        if(categoryModelList.size() < 2){
+            CategoryModel cm = new CategoryModel("Income", 0);
+            dbHandler.addCategory(cm);
+            categoryModelList.add(cm);
+            cm = new CategoryModel("Expenditure", 0);
+            dbHandler.addCategory(cm);
+            categoryModelList.add(cm);
+        }
 
         int[] incExp = getIncomeExpenditure(transactionList);
 
@@ -166,5 +160,17 @@ public class mainActivity extends AppCompatActivity {
             }
         }
         return incExp;
+    }
+
+    /**
+     * TODO : Fix this method in a new thread.
+     * Help method to update MainActivity's board display. Only supports quickAdd!!
+     */
+    private void updateMainDisplayBoard(int newExpenditure){
+        TextView expenditureNr = (TextView) findViewById(R.id.expenditureNr);
+        TextView totalNr = (TextView) findViewById(R.id.totalNr);
+
+        expenditureNr.setText(String.format("%s",Integer.parseInt(expenditureNr.getText().toString()) + newExpenditure));
+        totalNr.setText(String.format("%s",Integer.parseInt(totalNr.getText().toString()) + newExpenditure));
     }
 }
