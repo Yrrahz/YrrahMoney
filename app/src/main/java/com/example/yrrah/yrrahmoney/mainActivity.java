6ae7556908517243f3d9ade9ac46d1551824e36a
David Harald <David.Harald@sigma.se>
2018-01-19 08:27:19 +0100

    Slightly restructured MainActivity. Some of its methods were needed at multiple locations. OnResume was one.

--- app/src/main/java/com/example/yrrah/yrrahmoney/mainActivity.java
+++ app/src/main/java/com/example/yrrah/yrrahmoney/mainActivity.java
@@ -32,6 +32,12 @@ public class mainActivity extends AppCompatActivity {
         setup();
     }
 
+    @Override
+    protected void onResume() {
+        updateMainDisplayBoard(-1, true);
+        super.onResume();
+    }
+
     public void monthButtonPressed(View view){
         // Populate listview in mainActivity.
         // Take a chosen month and show all categories and their value for that month
@@ -74,23 +80,16 @@ public class mainActivity extends AppCompatActivity {
             quickAddNr.setText("");
             Toast.makeText(getApplicationContext(),spinnerCategory + " added!", Toast.LENGTH_SHORT).show();
 
-            updateMainDisplayBoard(quickAddInt);
+            updateMainDisplayBoard(quickAddInt, false);
         }else{
             Toast.makeText(getApplicationContext(),"No value added", Toast.LENGTH_SHORT).show();
         }
     }
 
     private void setup(){
-        TextView incomeNr = (TextView) findViewById(R.id.incomeNr);
-        TextView expenditureNr = (TextView) findViewById(R.id.expenditureNr);
-        TextView totalNr = (TextView) findViewById(R.id.totalNr);
-
-        Spinner dropDownList = (Spinner) findViewById(R.id.quickAddSpinner);
-
         DBHandler dbHandler = new DBHandler(this);
 
         List<CategoryModel> categoryModelList = dbHandler.getAllCategories();
-        List<SubAmountModel> transactionList = dbHandler.getAllSubAmounts();
 
         if(categoryModelList.size() < 2){
             CategoryModel cm = new CategoryModel("Income", 0);
@@ -101,29 +100,7 @@ public class mainActivity extends AppCompatActivity {
             categoryModelList.add(cm);
         }
 
-        int[] incExp = getIncomeExpenditure(transactionList);
-
-        incomeNr.setText(String.format("%s",incExp[0]));
-        expenditureNr.setText(String.format("%s",incExp[1]));
-        totalNr.setText(String.format("%s",dbHandler.totalAmount()));
-
-
-        // Populate the DropDown eventchoice list
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,convertCategoryList(categoryModelList));
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropDownList.setAdapter(adapter);
-
-        dropDownList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                spinnerCategory = parent.getSelectedItem().toString();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                spinnerCategory = null;
-            }
-        });
+        updateMainDisplayBoard(-1, true);
     }
 
     /**
@@ -164,13 +141,45 @@ public class mainActivity extends AppCompatActivity {
 
     /**
      * TODO : Fix this method in a new thread.
-     * Help method to update MainActivity's board display. Only supports quickAdd!!
+     * TODO : Check that this is not run 2 times on onCreate()
+     * Help method to update MainActivity's board display.
      */
-    private void updateMainDisplayBoard(int newExpenditure){
+    private void updateMainDisplayBoard(int newExpenditure, boolean updateAll){
+        TextView incomeNr = (TextView) findViewById(R.id.incomeNr);
         TextView expenditureNr = (TextView) findViewById(R.id.expenditureNr);
         TextView totalNr = (TextView) findViewById(R.id.totalNr);
 
-        expenditureNr.setText(String.format("%s",Integer.parseInt(expenditureNr.getText().toString()) + newExpenditure));
-        totalNr.setText(String.format("%s",Integer.parseInt(totalNr.getText().toString()) + newExpenditure));
+        if (updateAll) {
+            DBHandler dbHandler = new DBHandler(this);
+            List<CategoryModel> categoryModelList = dbHandler.getAllCategories();
+            List<SubAmountModel> transactionList = dbHandler.getAllSubAmounts();
+
+            int[] incExp = getIncomeExpenditure(transactionList);
+
+            incomeNr.setText(String.format("%s",incExp[0]));
+            expenditureNr.setText(String.format("%s",incExp[1]));
+            totalNr.setText(String.format("%s",dbHandler.totalAmount()));
+
+            Spinner dropDownList = (Spinner) findViewById(R.id.quickAddSpinner);
+            // Populate the DropDown eventchoice list
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,convertCategoryList(categoryModelList));
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dropDownList.setAdapter(adapter);
+
+            dropDownList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    spinnerCategory = parent.getSelectedItem().toString();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                    spinnerCategory = null;
+                }
+            });
+        } else {
+            expenditureNr.setText(String.format("%s",Integer.parseInt(expenditureNr.getText().toString()) + newExpenditure));
+            totalNr.setText(String.format("%s",Integer.parseInt(totalNr.getText().toString()) + newExpenditure));
+        }
     }
 }
